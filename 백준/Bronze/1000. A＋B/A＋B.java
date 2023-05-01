309f9ecbb65448d405808ce2742f426cc5b94df4
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-01 18:05:25 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int iVal1 = sc.nextInt();
+        int iVal2 = sc.nextInt();
+        
+        System.out.println(iVal1+iVal2);
+    }
+}
