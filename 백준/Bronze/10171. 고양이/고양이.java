0b413367b9e68b9760871476bbcdbfaa3d8f02a6
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-03 00:46:09 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 텍스트블록으로 풀고 싶었으나.. 
+        // 백준에서는 java11로 풀어야해서 사용하지 못했다
+        /*
+    String cat =
+"""
+\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|            
+""";        */
+        System.out.println("\\    /\\\n"+" )  ( ')\n"+"(  /  )\n"+" \\(__)|");
+    }
+}
