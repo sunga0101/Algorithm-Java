c25dfda717e91275c07361930c2e730560b79613
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-03 19:45:16 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 396 ms, Memory: 18672 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,25 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N =sc.nextInt();
+        for (int i = 1; i<=N; i++) {
+            for (int j = N-1-i; j >= 0; j--) {
+                System.out.print(" ");
+            }
+            
+            for ( int k = 0;k<2*i-1 ;k++ ) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            }
+        }
+
+    }
