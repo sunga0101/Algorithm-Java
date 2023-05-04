7d846fee9b184e2ac1205e6c35d97b70a09e54ba
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-04 09:26:35 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 592 ms, Memory: 23628 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N-1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 2 * N - 1 - 2 * i; k > 0; k--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - 1 - i; j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
