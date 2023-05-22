41b1d77c4b6ef480b02e8b98adc56e459ae7afa9
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-22 23:35:19 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
