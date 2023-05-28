23b1bed2bbb4994c4af637f6d71ea489f51828d6
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-29 00:23:48 +0900

    [Silver II] Title: N과 M (10), Time: 148 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,49 @@
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static boolean[] isused = new boolean[10]; // 특정 수가 쓰였는지 체크
+    static int[] arr = new int[10]; // 수열 담을 배열
+    static int m;
+    static int n;
+    static int[] nArr; // n개의 입력받은 수열
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]); //  n개의 수 중에
+        m = Integer.parseInt(s[1]); //  m개를 골라라
+        s = br.readLine().split(" ");
+        nArr = new int[n+1];
+        for (int i = 0; i < n; i++) {
+            nArr[i+1] = Integer.parseInt(s[i]); // nArr[1]부터 수열
+        }
+        Arrays.sort(nArr); //사전순 정렬
+        func(1,0);
+        bw.flush();
+        br.close();
+    }
+
+    public static void func(int index, int k) throws IOException { //nArr에서 m개
+        // base condition
+        if (k == m) { // m개가 되면 출력
+            for (int i = 0; i < m; i++) {
+                bw.write(arr[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+        int last = 0; // 가장 마지막 최근에 뽑은 수
+        for (int i =  index; i <= n; i++) { // nArr의 모든 요소를 arr에, index~n
+            if(!isused[i] && last!=nArr[i] ) { // 이전에 뽑았던 수가 아니어야 중복 아닌
+                arr[k] = nArr[i]; // k번째 결정
+                last = nArr[i];
+                isused[i] = true;
+                func(i,k + 1);
+                isused[i]=false;
+            }
+        }
+    }
+}
