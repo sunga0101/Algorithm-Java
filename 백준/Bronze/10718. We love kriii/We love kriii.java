23bad3daa6aec1a72f5ad3dd340b4a683b81f25c
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-06-11 23:17:36 +0900

    [Bronze V] Title: We love kriii, Time: 104 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public void solve() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("강한친구 대한육군\n강한친구 대한육군");
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main code = new Main();
+        code.solve();
+    }
+}
