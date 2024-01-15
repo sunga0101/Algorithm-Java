9f7bc43f19c89392138d09cfb8a968302383c22f
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-01-15 19:20:12 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 128 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,17 +1,23 @@
 
+
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int iLoof = sc.nextInt();
-        for (int j = 0; j < iLoof; j++) { // j = 몇 층
-            for (int i = iLoof-j; i >= 1 ; i--) { // reverse 층 수 만큼 별 찍기
-                System.out.print("*");
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            // 공백
+            for (int j = 0; j < N-(i); j++) {
+                bw.write("*");
             }
-            System.out.print("\n");
+            bw.write("\n");
         }
+        bw.flush();
+        br.close();
     }
 }
-
