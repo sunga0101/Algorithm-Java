b3492d4395c450e42a3cc0da3953ef5bf05a6bed
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-01-17 12:51:05 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,27 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            // " "
+            for (int k=0;k<N-(i+1);k++){
+                bw.write(" ");
+            }
+            // *
+            for (int j=0;j<i+1;j++){
+                bw.write("* ");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        br.close();
+    }
+}
