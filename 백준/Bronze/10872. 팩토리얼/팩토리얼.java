ae1465ddcac6ffab08b51738e9bf890078f27e1e
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-01-18 12:24:14 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,20 @@
+
+
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum=1;
-        int N = Integer.parseInt(br.readLine());
-        if (N>0){
-        for (int i= N; i>= 1; i--){
-            sum *= i;}}
-        else sum=1;
-        System.out.print(sum);
-        }
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int res = 1;
+        for (int i=1;i<N+1;i++){
+            res *= i;
+        }
+        bw.write(res+"");
+        bw.flush();
+        br.close();
     }
+}
