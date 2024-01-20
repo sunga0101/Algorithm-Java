761e67c431216c0c69a9be1fbc18607d63b12adb
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-01-20 17:42:00 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,32 +1,40 @@
 
-import java.util.Scanner;
-import java.util.Stack;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static Stack<String> stack;
-    public static String check(String[] s){
-        for (int j = 0; j < s.length; j++) {
-            if (s[j].equals("(")) {
-                stack.push(s[j]);
-            }
-            else if  (stack.isEmpty()) {
-                return "NO";
 
-            } else {
-                stack.pop();
-            }
-        }
-        if (stack.isEmpty()) return "YES";
-        else return "NO";
-    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            stack = new Stack<>();
-            String[] s = sc.next().split("");
-            System.out.println(check(s));
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++){
+            String res = "YES";
+            String s = br.readLine();
+            int len = s.length();
+
+            Stack<Integer> st = new Stack<>();
+            for (int j=0;j<len;j++){
+                if (s.charAt(j) == '(')
+                    st.push(1);
+                else {
+                    if (!st.isEmpty())
+                        st.pop();
+                    else {
+                        res = "NO";
+                        break;
+                    }
+                }
+            }
+            if (!st.isEmpty()) res = "NO";
+            bw.write(res+"\n");
+
         }
+
+        bw.flush();
+        br.close();
+
     }
 }
