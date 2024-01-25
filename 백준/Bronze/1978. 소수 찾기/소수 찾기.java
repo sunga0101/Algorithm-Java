5577db50b39bdf389253f495f901985d0712bc3a
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-01-25 19:09:42 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,31 +1,40 @@
 
+
 import java.io.*;
+import java.util.*;
+import java.math.*;
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        String[] s = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(s[i]);
-        }
-        int primeCnt = 0;
-        for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            for (int j = 2; j < arr[i]; j++) {
-                if (arr[i] != 1 && arr[i] % j == 0)
-                    break;
-                else {
-                    cnt++;
+        int T = Integer.parseInt(br.readLine());
+        String[] split = br.readLine().split(" ");
+        boolean[] check = new boolean[1001]; // 1~1000이하
+        check[0] = check[1] = true;
+        int cnt = 0;
+
+        for (int i=2;i<=1000;i++){ // 1000이하의 소수체크
+            if (!check[i]){
+                for (int j=i+i;j<=1000;j+=i){
+                    check[j] = true;
                 }
             }
-            if (cnt == arr[i] - 2) {
-                primeCnt++;
-            }
         }
-        bw.write(primeCnt + "\n");
+
+        for (int i=0;i<T;i++){
+            if (!check[Integer.parseInt(split[i])])
+                cnt++;
+        }
+
+        bw.write(cnt+"");
         bw.flush();
         br.close();
+
     }
 }
+/*
+4
+1 3 5 7
+*/
