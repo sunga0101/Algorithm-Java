f0e15664843d8a624a4bc004132d42bec0e52f4f
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-02-19 17:56:08 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 22216 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,39 +1,38 @@
 
+
 import java.io.*;
 import java.util.*;
-
+import java.math.*;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static long[] arr;
+
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        arr = new long[1000001];
+        long[] arr = new long[1000001];
         arr[0] = arr[1] = 0;
 
-        for (int i=2;i<=n;i++) {
-
-            // 6으로 나눌 경우
-            if (i%6==0){
-                arr[i] = Math.min(arr[i-1],(Math.min(arr[i/2],arr[i/3])))+1;
+        for (int i=2;i<=1000000;i++){
+            if (i%6==0){ // 6로 나눠지면
+                arr[i] = Math.min(arr[i-1],Math.min(arr[i/2],arr[i/3]))+1;
             }
-            // 3으로 나눌 경우
-            else if (i%3==0){
-                arr[i] = Math.min(arr[i-1],arr[i/3])+1;
-            }
-            // 2으로 나눌 경우
-            else if (i%2==0){
+            else if (i%2==0){ // 2로 나눠지면
                 arr[i] = Math.min(arr[i-1],arr[i/2])+1;
             }
-            // -1할 경우
-            else {
-                arr[i] = arr[i-1] + 1;
+            else if (i%3==0){ // 3로 나눠지면
+                arr[i] = Math.min(arr[i-1],arr[i/3])+1;
+            }
+            else { // 안나눠질때
+                arr[i] = arr[i-1]+1;
             }
         }
         bw.write(arr[n]+"");
         bw.flush();
         br.close();
     }
+
+
 }
+
