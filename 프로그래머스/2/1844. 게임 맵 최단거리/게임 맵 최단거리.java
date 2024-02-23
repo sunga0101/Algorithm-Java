bb6640cd8d7d419f05d0f0d63acfef8cce3e3fd7
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2024-02-24 01:12:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.28 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,62 @@
 import java.util.*;
 
 class Solution {
-    static int answer = -1;
-    static boolean check[][];
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,-1,0,1};
-    
+    static boolean[][] visited;
+    static int m,n;
     public int solution(int[][] maps) {
-        check = new boolean[maps.length][maps[0].length];
-        bfs(maps);
-        return answer;
+        m = maps.length;
+        n = maps[0].length;
+        visited = new boolean[m][n];
+        return BFS(maps);
     }
     
-    public void bfs(int[][] maps){
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0,0,1));
-        check[0][0] = true;
+    public int BFS(int[][] maps){
+        int result = Integer.MAX_VALUE;
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        int cnt = -1;
+        if (maps[0][0] != 1) return -1;
+        if (maps[m-1][n-1] != 1) return -1;
         
-        while (!q.isEmpty()){
-            Node cur = q.poll();
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0,0));
+        visited[0][0]=true;
+        maps[0][0] = 1;
+        
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            int x = node.i;
+            int y = node.j;
             
-            if (cur.x == maps.length-1 && 
-                cur.y ==maps[0].length-1){
-                answer = cur.count;
-                return;
+            if (x==m-1 && y==n-1 && maps[x][y] == 0) {
+                return -1;
             }
             
-            for (int i = 0; i<4; i++){
-                int nx = cur.x+dx[i];
-                int ny = cur.y+dy[i];
+            for (int k=0;k<4;k++){
+                int nx = x+dx[k];
+                int ny = y+dy[k];
                 
-                if (nx>=0&& nx < maps.length && 
-                    ny>=0&& ny <maps[0].length && 
-                    !check[nx][ny] && 
-                    maps[nx][ny]==1){
-                    
-                    check[nx][ny] = true;
-                    q.add(new Node(nx,ny, cur.count+1));
+                if (nx>=0&&nx<m&&ny>=0&&ny<n){
+                    if(!visited[nx][ny]&&maps[nx][ny]==1){
+                    q.offer(new Node(nx,ny));
+                    visited[nx][ny]=true;
+                    maps[nx][ny] = maps[x][y]+1;
+                    }
                 }
+                
             }
         }
-        
-        
-        
+        if (visited[m-1][n-1])    
+            return maps[m-1][n-1];
+        else return -1;
     }
-}
-
-class Node {
-    int x,y,count;
-    public Node(int x, int y, int count){
-        this.x = x;
-        this.y = y;
-        this.count = count;
+    
+    class Node{
+        int i;
+        int j;
+        public Node(int i,int j){
+            this.i=i;
+            this.j=j;
+        }
     }
 }
